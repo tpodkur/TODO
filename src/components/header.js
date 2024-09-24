9be8b39e8c26921531b776e7b2033a737195531e
Tatiana <t.podkur17@gmail.com>
2024-09-24 10:21:04 +0300

    make input field empty after submit

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,18 @@ import React, {Component} from 'react';
 
 export default class Header extends Component {
 
+  state = {
+    inputValue: ''
+  }
+
+  onChange = (event) => {
+    this.setState({ inputValue: event.target.value });
+  };
+
   onSubmit = (event) => {
     event.preventDefault();
     this.props.addTask(event.target.querySelector('.new-todo').value);
+    this.setState({ inputValue: '' });
   };
 
   render() {
@@ -16,6 +25,8 @@ export default class Header extends Component {
             className="new-todo"
             placeholder="What needs to be done?"
             autoFocus
+            onChange={ this.onChange }
+            value={ this.state.inputValue }
           />
         </form>
       </header>
