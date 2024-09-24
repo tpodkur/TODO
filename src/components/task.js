60b90e80784852cb6bfa4f17ddd8381dd78e46b9
Tatiana <t.podkur17@gmail.com>
2024-09-24 08:10:36 +0300

    make Task component functional

--- src/components/task.js
+++ src/components/task.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Task extends Component {
-  render() {
-    return (
-    <React.Fragment>
-      <div className="view">
-        <input
-          className="toggle"
-          type="checkbox"
-          onChange={ this.props.onChangeClassname }
-          checked={ this.props.className === 'completed' }
-        />
-        <label>
-          <span className="description">{ this.props.description }</span>
-          <span className="created">{ this.props.created }</span>
-        </label>
-        <button className="icon icon-edit"></button>
-        <button className="icon icon-destroy" onClick={ this.props.onDeleteTask }></button>
-      </div>
-      { this.props.className === 'editing' && <input type='text' className='edit' defaultValue='Editing task'/> }
-    </React.Fragment>
-    );
-  }
+const Task = ({ className, description, created, onChangeClassname, onDeleteTask }) => {
+  return (
+  <React.Fragment>
+    <div className="view">
+      <input
+        className="toggle"
+        type="checkbox"
+        onChange={ onChangeClassname }
+        checked={ className === 'completed' }
+      />
+      <label>
+        <span className="description">{ description }</span>
+        <span className="created">{ created }</span>
+      </label>
+      <button className="icon icon-edit"></button>
+      <button className="icon icon-destroy" onClick={ onDeleteTask }></button>
+    </div>
+    { className === 'editing' && <input type='text' className='edit' defaultValue='Editing task'/> }
+  </React.Fragment>
+  );
 }
+
+export default Task;
